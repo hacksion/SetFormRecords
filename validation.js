9760eb1d3be68c16660cfea19b7c882a7ea91f4b
M.Toshi <33745694+hacksion@users.noreply.github.com>
2023-07-27 12:57:02 +0900

    Update validation.js

--- validation.js
+++ validation.js
@@ -30,3 +30,24 @@ const emptyValid = e => {
 const htmlTagsRep = e => {
     e.value = e.value.replace(/(<([^>]+)>)/gi, '');
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    (() => {
+        [].slice.call(document.querySelectorAll('.needs-validation')).forEach(form => {
+            form.addEventListener('submit', e => {
+                if (!form.checkValidity()) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false)
+        })
+    })();
+
+});
+
+/*
+
+ onblur="emptyValid(this); htmlTagsRep(this)"
+*/
